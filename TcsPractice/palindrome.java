ef290a9bc9f596bfcf5c8b841ed4cb3417f1607c
Vaibhav Manchanda <manchandavaibhav915@gmail.com>
2026-03-16 16:50:00 +0530

    Starting StecondLargest

---
+++ TcsPractice/palindrome.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class palindrome{
+    public static void main( String[] args){
+        palindrome obj = new palindrome();
+        System.out.println(obj.check(6556));
+    }
+    public boolean check(int n){
+        int rem=0, digits=0, temp=n;
+        boolean result = false;
+        while(temp>0){
+            rem = temp%10;
+            digits = digits * 10 + rem;
+            temp=temp/10;
+        }
+        if( digits == n)
+            result = true ;
+        return result;
+    }
+}
---
+++ TcsPractice/revNumb.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class revNumb{
+    public static void main (String[] args){
+        revNumb ob = new revNumb();
+        int reversed=ob.rev(456);
+        System.out.print("reversed" + reversed);
+    }
+    
+    public int rev (int n){
+        int rem=0, digit=0;
+        while(n>0){
+            rem=n%10;
+            digit=digit*10+rem;
+            n=n/10;
+        }
+        return digit;
+    }
+}
