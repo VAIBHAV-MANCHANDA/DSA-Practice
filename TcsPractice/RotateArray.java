55991b18adf0ab7e8bdce5e14dd399131746e4dd
Vaibhav Manchanda <manchandavaibhav915@gmail.com>
2026-03-24 10:30:59 +0530

    second method

--- TcsPractice/RotateArray.java
+++ TcsPractice/RotateArray.java
@@ -1,37 +1,81 @@
 public class RotateArray {
     public static void main(String[] args){
-        int arr[]={1, 2, 3, 4, 5};
-        int k=2;
+        int arr[]={1, 2, 3, 4, 5, 6, 7, 8};
+        int k=3;
         RotateArray ob = new RotateArray();
-        int[] result = ob.rotation(arr, k);
-
-        for(int num : result){
-            System.out.print(num + " ");
-        }
+        ob.rotation(arr,k);
     }
-    public int[] rotation(int arr[], int k){
-        int l= arr.length,temp;
-        for(int i=0; i<l/2; i++){
-            temp=arr[i];
-            arr[i]= arr[l-i-1];
-            arr[l-i-1]=temp;
-        }
-        for(int i=0; i<k; i++){
-            temp=arr[i];
-            arr[i]= arr[k-i-1];
-            arr[k-i-1]=temp;
-        }
+
+    public void rotation(int arr[], int k){
+        int l = arr.length;
+        k= k % l; // handle large inputs 
         
-       int start = k, end = l - 1;
+        // reverse whole array
+        reverse(arr,0, l-1);
+
+        // reverse first k elements
+        reverse(arr, 0, k-1);
+
+        // reverse remaining elements 
+        reverse(arr, k, l-1);
+
+        for(int i=0; i<l; i++){
+            System.out.print(arr[i]);
+        }
+
+    }
+
+
+
+    public void reverse(int arr[], int start, int end){
+        int temp;
         while(start < end){
-            temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
             start++;
             end--;
-        
         }
-        return arr;
     }
 }
+
+
+
+
+
+
+
+
+//         RotateArray ob = new RotateArray();
+//         int[] result = ob.rotation(arr, k);
+
+//         for(int num : result){
+//             System.out.print(num + " ");
+//         }
+//     }
+//     public int[] rotation(int arr[], int k){
+//         int l= arr.length,temp;
+//         for(int i=0; i<l/2; i++){
+//             temp=arr[i];
+//             arr[i]= arr[l-i-1];
+//             arr[l-i-1]=temp;
+//         }
+//         for(int i=0; i<k; i++){
+//             temp=arr[i];
+//             arr[i]= arr[k-i-1];
+//             arr[k-i-1]=temp;
+//         }
+        
+//        int start = k, end = l - 1;
+//         while(start < end){
+//             temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end] = temp;
+//             start++;
+//             end--;
+        
+//         }
+//         return arr;
+//     }
+// }
 
